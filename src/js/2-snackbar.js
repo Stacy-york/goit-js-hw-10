9483ecf269a7d67b0c392504264c08abc0ccaa89
Stacy York <stacyyyork@gmail.com>
2025-03-09 17:09:36 +0200

    added snackbar, completed task 2

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,40 @@
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+  const delayInput = document.querySelector('input[name="delay"]');
+  const delay = delayInput.value; 
+  const state = document.querySelector('input[name="state"]:checked').value;
+
+  delayInput.value = ''; 
+    
+    const promise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (state === 'fulfilled') {
+                resolve(delay);
+            } else {
+                reject(delay);
+            }
+        }, delay);
+    });
+
+    promise
+        .then((delay) => {
+            iziToast.success({
+                title: 'Success',
+                message: `✅ Fulfilled promise in ${delay}ms`,
+                position: 'topRight'
+            });
+        })
+        .catch((delay) => {
+            iziToast.error({
+                title: 'Error',
+                message: `❌ Rejected promise in ${delay}ms`,
+                position: 'topRight'
+            });
+        });
+});
